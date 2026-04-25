9ac7ed4cbdfc2c67fd53c77eacf69c2c389c2f76
galacticnuclei <mihirmrao@gmail.com>
2026-04-25 18:28:29 +0530

    Added node file

---
+++ node/node.go
@@ -0,0 +1,15 @@
+package node
+
+type Role string
+
+const (
+    Leader   Role = "leader"
+    Follower Role = "follower"
+)
+
+type Node struct {
+    ID    string
+    Port  string
+    Peers []string
+    Role  Role
+}
